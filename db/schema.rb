fdde1edd57b3d4a7510c83b70970e445e937b08a
antonio <anodari@gmail.com>
2011-03-20 10:58:51 -0300

    Finished first cut of the User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110317101726) do
+ActiveRecord::Schema.define(:version => 20110320131305) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
