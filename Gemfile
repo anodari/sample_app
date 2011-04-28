67c6254b7bf38dac2035b8a85f4dfd36e83ea188
antonio <anodari@gmail.com>
2011-04-28 07:34:34 -0300

    edit, update, destroy, list users

--- Gemfile
+++ Gemfile
@@ -8,14 +8,14 @@ gem 'rails', '3.0.0'
 #gem 'sqlite3-ruby', :require => 'sqlite3'
 
 gem 'sqlite3-ruby', '1.3.2', :require => 'sqlite3'
-
 gem 'gravatar_image_tag', '1.0.0.pre2'
-
+gem 'will_paginate'
 gem 'pg'
 
 group :development do
   gem 'rspec-rails', '2.5.0'
   gem 'annotate-models', '1.0.4'
+  gem 'faker', '0.3.1'
 end
 
 group :test do
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 module SessionsHelper
 
   def sign_in(user)
@@ -22,6 +23,20 @@ module SessionsHelper
     @current_user ||= user_from_remember_token
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
+  def deny_access
+  	store_location
+    redirect_to signin_path, :notice => "Faça o login para acessar esta página."
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
+  end
+
   private
 
     def user_from_remember_token
@@ -30,6 +45,14 @@ module SessionsHelper
 
     def remember_token
       cookies.signed[:remember_token] || [nil, nil]
+    end
+
+    def store_location
+      session[:return_to] = request.fullpath
+    end
+
+    def clear_return_to
+      session[:return_to] = nil
     end
 
 end
